9c09ada2cfc076e87ecb82bd1dc7d9e7a9993058
Avishka Kavinda <45445179+acekavi@users.noreply.github.com>
2021-01-24 22:22:09 +0530

    Added working list tag endpoints

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test2test.com', password='testpass'):
+    # Create a sample user
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTest(TestCase):
@@ -38,3 +44,12 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        # Test the tags string representation
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertTrue(str(tag), tag.name)
---
+++ app/recipe/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+#  from rest_framework import urlpatterns
+from rest_framework.routers import DefaultRouter
+#  from rest_framework.schemas import views
+
+from recipe import views
+
+
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
