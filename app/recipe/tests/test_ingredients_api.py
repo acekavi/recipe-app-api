a09fbb903692ea8dfdb880ce08fa09f6afd61a79
Avishka Kavinda <45445179+acekavi@users.noreply.github.com>
2021-01-25 15:55:22 +0530

    Added ingredient create function

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -65,21 +65,21 @@ class PrivateIngredientsAPITest(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
 
-    # def test_create_tag_successful(self):
-    #     # Test creating a new tag
-    #     payload = {'name': 'Test Tag'}
-    #     self.client.post(TAGS_URL, payload)
+    def test_create_ingredient_successful(self):
+        # Test creating a new tag
+        payload = {'name': 'Test Ingredient'}
+        self.client.post(INGREDIENTS_URL, payload)
 
-    #     exists = Tag.objects.filter(
-    #         user=self.user,
-    #         name=payload['name']
-    #     ).exists()
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
 
-    #     self.assertTrue(exists)
+        self.assertTrue(exists)
 
-    # def test_create_tag_invalid(self):
-    #     # Testing creating a tag with invalid payload
-    #     payload = {'name': ''}
-    #     res = self.client.post(TAGS_URL, payload)
+    def test_create_ingredient_invalid(self):
+        # Testing creating a tag with invalid payload
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
 
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
